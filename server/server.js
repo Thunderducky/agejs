6dca4edb1b6d1179ebd301c8ce6d15864947a84c
Eric M Scott <thunderducky@Erics-MacBook-Pro.local>
2017-09-25 17:00:03 -0500

    fixed heroku commit issue (ports)

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require("Express");
 
 const app = express();
+var port = process.env.PORT || 3000;
 app.use('/public/', express.static("server/public"));
 app.get('/', function(req, res){
   res.redirect("/public/html/index.html");
 })
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
+app.listen(port, function(){
+  console.log("Listening on port " + port);
 });
